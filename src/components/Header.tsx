55b97860d72cf152b3b2438528986fb7eed1f93c
waseem <waseemazmy21@gmail.com>
2024-02-13 02:48:41 +0200

    Create the Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,25 @@
+import { Link } from 'react-router-dom';
+import MobileMenu from './MobileMenu';
+import Cart from './Cart';
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase md:h-16 lg:px-12">
+      {/* Logo */}
+      <div className="text-xl md:font-bold">
+        <Link to="/">Massimo</Link>
+      </div>
+
+      <MobileMenu />
+
+      {/* Right Links */}
+      <div className="hidden md:flex gap-4 items-center justify-end">
+        <Link to="/menu">Menu</Link>
+        <Link to="/orders">Orders</Link>
+        <Cart />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
---
+++ src/components/MobileMenu.tsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Cart from './Cart';
+
+const links = [
+  { id: 1, title: 'Homepage', url: '/' },
+  { id: 2, title: 'Menu', url: '/menu' },
+  { id: 3, title: 'Orders', url: '/orders' },
+] as const;
+
+const MobileMenu = () => {
+  const [open, setOpen] = useState(false);
+
+  function closeMenu() {
+    setOpen(false);
+  }
+  return (
+    <div className="md:hidden">
+      <button onClick={() => setOpen(!open)}>
+        <img
+          src={open ? 'close.png' : '/open.png'}
+          alt=""
+          className="w-5 h-5 object-cover"
+        />
+      </button>
+
+      {open && (
+        <div className="bg-red-500 text-white absolute left-0 top-24 w-full h-[calc(100vh-6rem)] flex flex-col justify-center text-3xl z-10">
+          {links.map((link) => (
+            <Link
+              key={link.id}
+              to={link.url}
+              onClick={closeMenu}
+              className="hover:bg-red-600 text-center p-4"
+            >
+              {link.title}
+            </Link>
+          ))}
+          <div
+            onClick={closeMenu}
+            className="flex justify-center p-4 hover:bg-red-600"
+          >
+            <Cart />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MobileMenu;
